0bb0c095d38654f3b837f1d0d46a0ae2e4628b62
AngryDeer <dimkin.pivovarov@gmail.com>
2015-12-18 18:55:37 +0200

    Исправлена migrations

--- database/migrations/2015_12_18_165345_DPS-CookBook.php
+++ database/migrations/2015_12_18_165345_DPS-CookBook.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableMarkers extends Migration
+class DPSCookBook extends Migration
 {
     /**
      * Run the migrations.
@@ -12,6 +12,7 @@ class CreateTableMarkers extends Migration
      */
     public function up()
     {
+
         Schema::create('markers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
