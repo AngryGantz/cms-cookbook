1a9659c867e8c49f0675a861470a368336d90630
AngryDeer <dimkin.pivovarov@gmail.com>
2015-12-26 13:14:17 +0200

    s

--- config/app.php
+++ config/app.php
@@ -158,7 +158,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-//        Conner\Tagging\Providers\TaggingServiceProvider::class,
+        Conner\Tagging\Providers\TaggingServiceProvider::class,
         /*
          * my providers
          */
