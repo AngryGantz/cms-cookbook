1fc8a4eb8e75ccec66835a920e72b29c96a7e484
AngryDeer <dimkin.pivovarov@gmail.com>
2015-12-27 18:17:48 +0200

    изменены социалки в рецепте

--- resources/views/recipie/_onerecipieTagsSocial.blade.php
+++ resources/views/recipie/_onerecipieTagsSocial.blade.php
@@ -6,9 +6,23 @@
  * Time: 12:45
  */?>
 
-<div class="tags-icons">
-    <div class="row">
-        <div class="col-sm-7">
+
+<script type="text/javascript">(function(w,doc) {
+        if (!w.__utlWdgt ) {
+            w.__utlWdgt = true;
+            var d = doc, s = d.createElement('script'), g = 'getElementsByTagName';
+            s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+            s.src = ('https:' == w.location.protocol ? 'https' : 'http')  + '://w.uptolike.com/widgets/v1/uptolike.js';
+            var h=d[g]('body')[0];
+            h.appendChild(s);
+        }})(window,document);
+</script>
+<div data-background-alpha="0.0" data-buttons-color="#ffffff" data-counter-background-color="#ffffff" data-share-counter-size="10" data-top-button="false" data-share-counter-type="common" data-share-style="7" data-mode="share" data-like-text-enable="false" data-mobile-view="true" data-icon-color="#ffffff" data-orientation="horizontal" data-text-color="#000000" data-share-shape="rectangle" data-sn-ids="fb.vk.tw.ok." data-share-size="20" data-background-color="#ffffff" data-preview-mobile="false" data-mobile-sn-ids="fb.vk.tw.wh.ok.vb." data-pid="1449864" data-counter-background-alpha="1.0" data-following-enable="false" data-exclude-show-more="false" data-selection-enable="true" class="uptolike-buttons" ></div>
+
+
+{{--<div class="tags-icons">--}}
+    {{--<div class="row">--}}
+        {{--<div class="col-sm-7">--}}
             {{--<div class="details-tags">--}}
                 {{--<ul>--}}
                     {{--<li><a href="#">garlic</a></li>--}}
@@ -17,19 +31,19 @@
                     {{--<li><a href="#">roasted</a></li>--}}
                 {{--</ul>--}}
             {{--</div>--}}
-        </div>
-        <div class="col-sm-5">
-            <div class="details-social-icons">
-                <ul>
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+        {{--</div>--}}
+        {{--<div class="col-sm-5">--}}
+            {{--<div class="details-social-icons">--}}
+                {{--<ul>--}}
+                    {{--<li><a href="#"><i class="fa fa-facebook"></i></a></li>--}}
+                    {{--<li><a href="#"><i class="fa fa-twitter"></i></a></li>--}}
+                    {{--<li><a href="#"><i class="fa fa-google-plus"></i></a></li>--}}
                     {{--<li><a href="#"><i class="fa fa-pinterest"></i></a></li>--}}
-                    <li><a href="#"><i class="fa fa-vk"></i></a></li>
-                    <li><a href="#"><i class="fa fa-odnoklassniki"></i></a></li>
+                    {{--<li><a href="#"><i class="fa fa-vk"></i></a></li>--}}
+                    {{--<li><a href="#"><i class="fa fa-odnoklassniki"></i></a></li>--}}
                     {{--<li><a href="#"><i class="fa fa-plus"></i></a></li>--}}
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
+                {{--</ul>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
--- resources/views/recipie/recipieSingle_body.blade.php
+++ resources/views/recipie/recipieSingle_body.blade.php
@@ -20,17 +20,6 @@
 {{--<div data-background-alpha="0.0" data-buttons-color="#ffffff" data-counter-background-color="#ffffff" data-share-counter-size="10" data-top-button="false" data-share-counter-type="common" data-share-style="7" data-mode="share" data-like-text-enable="false" data-mobile-view="true" data-icon-color="#ffffff" data-orientation="horizontal" data-text-color="#000000" data-share-shape="rectangle" data-sn-ids="fb.vk.tw.ok." data-share-size="20" data-background-color="#ffffff" data-preview-mobile="false" data-mobile-sn-ids="fb.vk.tw.wh.ok.vb." data-pid="1449864" data-counter-background-alpha="1.0" data-following-enable="false" data-exclude-show-more="false" data-selection-enable="true" class="uptolike-buttons" ></div>--}}
 
 
-{{--<script type="text/javascript">(function(w,doc) {--}}
-        {{--if (!w.__utlWdgt ) {--}}
-            {{--w.__utlWdgt = true;--}}
-            {{--var d = doc, s = d.createElement('script'), g = 'getElementsByTagName';--}}
-            {{--s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;--}}
-            {{--s.src = ('https:' == w.location.protocol ? 'https' : 'http')  + '://w.uptolike.com/widgets/v1/uptolike.js';--}}
-            {{--var h=d[g]('body')[0];--}}
-            {{--h.appendChild(s);--}}
-        {{--}})(window,document);--}}
-{{--</script>--}}
-{{--<div data-background-alpha="0.0" data-buttons-color="#ffffff" data-counter-background-color="#ffffff" data-share-counter-size="10" data-top-button="false" data-share-counter-type="common" data-share-style="7" data-mode="share" data-like-text-enable="false" data-mobile-view="true" data-icon-color="#ffffff" data-orientation="horizontal" data-text-color="#000000" data-share-shape="rectangle" data-sn-ids="fb.vk.tw.ok." data-share-size="20" data-background-color="#ffffff" data-preview-mobile="false" data-mobile-sn-ids="fb.vk.tw.wh.ok.vb." data-pid="1449864" data-counter-background-alpha="1.0" data-following-enable="false" data-exclude-show-more="false" data-selection-enable="true" class="uptolike-buttons" ></div>--}}
 
 
 
