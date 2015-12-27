08885f7caff81f46069724bb10b42d248c0445a7
AngryDeer <dimkin.pivovarov@gmail.com>
2015-12-27 02:25:24 +0200

    косяк с биндингом добавленных шагов в js

--- resources/views/footer/foot_script.blade.php
+++ resources/views/footer/foot_script.blade.php
@@ -31,48 +31,46 @@
 
 <script>
 
-    function bindMajesticItem(){
-
-        /* Bind click event to remove detail icon button */
-
-        $('.del-list').on("click",function(event){
-            event.preventDefault();
-            var $this = $( this );
-            $this.closest( 'li' ).slideUp(function() { $(this).remove(); });
-        });
-
-        $(".file-preview").on('click', function(){
-            $(this).siblings('input[type=file]').trigger('click');
-        });
-
-        $('input[type=file]').on('change', function(e) {
-            if(typeof FileReader == "undefined") return true;
-
-            var elem = $(this);
-            var files = e.target.files;
-
-            for (var i=0, file; file=files[i]; i++) {
-                if (file.type.match('image.*')) {
-                    var reader = new FileReader();
-                    reader.onload = (function(theFile) {
-                        return function(e) {
-                            var image = e.target.result;
-                            var previewDiv = $('.file-preview', elem.parent());
-                            var bgWidth = previewDiv.width() * 2;
-                            previewDiv.css({
-                                "background-size":bgWidth + "px, auto",
-                                "background-position":"50%, 50%",
-                                "background-image":"url("+image+")",
-                            });
-                        };
-                    })(file);
-                    reader.readAsDataURL(file);
-                }
-            }
-        });
-
-
-    }
+//    function bindMajesticItem(){
+//
+//        /* Bind click event to remove detail icon button */
+//
+//        $('.del-list').on("click",function(event){
+//            event.preventDefault();
+//            var $this = $( this );
+//            $this.closest( 'li' ).slideUp(function() { $(this).remove(); });
+//        });
+//
+//        $(".file-preview").on('click', function(){
+//            $(this).siblings('input[type=file]').trigger('click');
+//        });
+//
+//        $('input[type=file]').on('change', function(e) {
+//            if(typeof FileReader == "undefined") return true;
+//
+//            var elem = $(this);
+//            var files = e.target.files;
+//
+//            for (var i=0, file; file=files[i]; i++) {
+//                if (file.type.match('image.*')) {
+//                    var reader = new FileReader();
+//                    reader.onload = (function(theFile) {
+//                        return function(e) {
+//                            var image = e.target.result;
+//                            var previewDiv = $('.file-preview', elem.parent());
+//                            var bgWidth = previewDiv.width() * 2;
+//                            previewDiv.css({
+//                                "background-size":bgWidth + "px, auto",
+//                                "background-position":"50%, 50%",
+//                                "background-image":"url("+image+")",
+//                            });
+//                        };
+//                    })(file);
+//                    reader.readAsDataURL(file);
+//                }
+//            }
+//        });
+//    }
 
     $( document ).ready(function() {
         $(".file-preview").on('click', function(){
@@ -107,7 +105,7 @@
                 }
             }
         });
-        bindMajesticItem();
+//        bindMajesticItem();
     });
 </script>
 
