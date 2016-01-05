d9e17edcd944fac9ef89bcbccfb12e453ba69fcf
AngryDeer <dimkin.pivovarov@gmail.com>
2016-01-05 18:21:10 +0200

    добавлена своя 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmitriy Pivovarov aka AngryDeer http://studioweb.pro
+ * Date: 05.01.16
+ * Time: 18:18
+ */
+?>
+
+@extends('layouts.master')
+@section('body')
+    <div class="recipes-home-body inner-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-lg-9">
+                    <h3>Извините, такой страницы на сайте не существует</h3>
+                </div>
+                <div class="col-md-4 col-lg-3">
+                    @include('aside.aside')
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
